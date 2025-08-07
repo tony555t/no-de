191ed0c4a39a4fddce0c6283cae7c547c2bb39a0
tony555t <your-email@example.com>
2025-08-07 17:18:13 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const mathFunction = require('./math_Function');
+
+console.log(mathFunction.add(1,2));
+console.log(mathFunction.sub(20,10));
+
---
+++ math_Function.js
@@ -0,0 +1,17 @@
+// math_Function.js
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+// Export the functions
+module.exports = {
+     add,
+    sub
+};
+
+// Or using shorthand:
+// module.exports = { add, sub };
