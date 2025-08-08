dfb1ec82e5763c9013238c3b75b9044009bf3a43
tony555t <your-email@example.com>
2025-08-08 14:59:34 +0300

    folder

--- working-with-files/read.js
+++ working-with-files/read.js
@@ -1,1 +1,17 @@
-weer
+const fs = require('fs')
+const path = require('path')
+
+const poemFilePath = path.join(__dirname, 'files','poem.txt')
+const countriesFilePath = path.join(__dirname,'files','countries.json')
+
+fs.readFile(poemFilePath,'utf8',(err,data)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log("file raad successfully")
+    console.log(data)
+})
+//  read a file synchronously
+const poemData = fs.readFileSync(countriesFilePath,'utf8')
+// console.log(poemData)
