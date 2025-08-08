29a3b5fc812cba0a8e083ba179de67259389c66b
tony555t <your-email@example.com>
2025-08-08 12:34:33 +0300

    folder

---
+++ working-with-files/folder.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const path =require('path')
+
+const folderPath = path.join(__dirname,'file')
+fs.readdir( folderPath,(err,file)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(files)
+    console.log('folder opened successfully')
+})
+
+//create a folder
+const newFolderPath = path.join(__dirname,'file','new_folder')
+fs.mkdir(newFolderPath,(err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('folder crested successfully')
+})
+//
+const newFolder = path.join(__dirname,'file','new_folder_renamed')
+fs.rename(newFolderPath,newFolder,(err)=>{
+    if(err){
+        console(err)
+        return
+    }
+    console.log('folder renamed succesfully')
+})
+
+/// delete folderr
+fs.rmdir(newFolder,(err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('folder delete succesfully')
+})
+ 
