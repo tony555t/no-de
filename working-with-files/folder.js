ac0e623e8d0473c4323568a0f5c7107e16d8ef19
tony555t <your-email@example.com>
2025-08-08 12:50:33 +0300

    folder

--- working-with-files/folder.js
+++ working-with-files/folder.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
-const path =require('path')
+const path = require('path')
 
-const folderPath = path.join(__dirname,'file')
-fs.readdir( folderPath,(err,file)=>{
+// Read the 'files' directory that exists in your project
+const folderPath = path.join(__dirname,'files')
+fs.readdir(folderPath,(err,files)=>{
     if(err){
         console.log(err)
         return
@@ -11,31 +12,31 @@ fs.readdir( folderPath,(err,file)=>{
     console.log('folder opened successfully')
 })
 
-//create a folder
-const newFolderPath = path.join(__dirname,'file','new_folder')
-fs.mkdir(newFolderPath,(err)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('folder crested successfully')
-})
-//
-const newFolder = path.join(__dirname,'file','new_folder_renamed')
-fs.rename(newFolderPath,newFolder,(err)=>{
-    if(err){
-        console(err)
-        return
-    }
-    console.log('folder renamed succesfully')
-})
+// //create a folder
+// const newFolderPath = path.join(__dirname,'file','new_folder')
+// fs.mkdir(newFolderPath,(err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log('folder crested successfully')
+// })
+// //
+// const newFolder = path.join(__dirname,'file','new_folder_renamed')
+// fs.rename(newFolderPath,newFolder,(err)=>{
+//     if(err){
+//         console(err)
+//         return
+//     }
+//     console.log('folder renamed succesfully')
+// })
 
-/// delete folderr
-fs.rmdir(newFolder,(err)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('folder delete succesfully')
-})
+// /// delete folderr
+// fs.rmdir(newFolder,(err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log('folder delete succesfully')
+// })
  
