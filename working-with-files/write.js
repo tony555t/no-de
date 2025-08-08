81fbec1b55d74d1b8985172d62b7be53f9310c2f
tony555t <your-email@example.com>
2025-08-08 15:32:43 +0300

    new write file

---
+++ working-with-files/write.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const { text } = require('stream/consumers')
+
+const textFilePath = path.join(__dirname, 'files','new.txt')
+ fs.writeFile(textFilePath,content,(err)=>{
+    if (err){
+        console.log(err)
+    }
+console.log('file written succesfully')
+ })
+ 
+
+//  append to afile 
+ fs.appendFile(textFilePath,'|n this is a thid line ',(err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('adddition to the file ')
+ })
