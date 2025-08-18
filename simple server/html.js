b4d44bbaed0a0962b6b5f820b4727be884b1e626
tony555t <your-email@example.com>
2025-08-18 16:02:28 +0300

    added status header

--- simple server/html.js
+++ simple server/html.js
@@ -6,6 +6,11 @@ const PORT = 3000
 
 function requestHandler(req,res){
     console.log(req)
+
+    res.statusHeader(200,{'Content-Type':'text/plain',
+                          'Acces-content-Allow-Origin':"*"
+
+    })
     res.write( "this was made from the scrtch\n");
     res.write ("sending greetings\n");
     res.end("hello from the server\n")
@@ -14,4 +19,6 @@ const server = http.createServer(requestHandler)
 server.listen(PORT, HOSTNAME, () => {
     console.log(`server started succefully at http://${HOSTNAME}:${PORT}`)
 })
-
+    'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+    });
