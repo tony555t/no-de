10aa1b6c7e7e431e84032c3abd1bd25cdeefb8ca
tony555t <your-email@example.com>
2025-08-19 12:25:09 +0300

    db connection

--- server-methods/server.js
+++ server-methods/server.js
@@ -2,4 +2,31 @@ const http = require('http');
 const fs = require('fs');
 const path = require ('path');
 
+const booksDbpath = path.join(__dirname, "db",'books.json');
+    let booksDB = [];
+
+const PORT = 4000
+const HOST_NAME =  'localhost';
+
+
+const requestHandler = function(req,res){
+    res. setHeader("content-Type","application/json");
+     
+    if (req.url ==='./books'&& req.method === 'GET'){
+        getAllBooks(req, res);
+    }
+    else if( req.url ==='/books' && req.method === 'POST'){
+        addBook(req, res);
+
+    }else if( req.url.startsWith('/books') && req.method === 'DELETE'){
+        deleteBook(req,res);
+
+    }else{
+        res.writeHeader(404);
+        res.end(json.stringify({
+            message:'message not supported'
+        }))
+    }
+}
+
 
