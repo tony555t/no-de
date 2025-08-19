02a563bb3fe5d4af04f53f7960e59a502e630f27
tony555t <your-email@example.com>
2025-08-19 15:59:34 +0300

    function request handler

--- server-methods/book_server.js
+++ server-methods/book_server.js
@@ -9,5 +9,16 @@ const http = require ('http');
  const HOST_NAME = 'localhost';
 
  function requestHandler (req,res){
-    
+    if (req.url === '/books'&& req.method === "GET"){
+        // READ THE LOAD AND RETURN BOOKS
+        getAllBooks(req,res)
+
+    }else if(req.url === '/books'&& req.method ==="post"){
+        addBook(req,res)
+    }else if (req.url === '/books'&& req.method === "PUT"){
+        updateBook(req,res)
+    }else if (req.url === '/books' && req.method === "DELETE"){
+        deleteBook(req,res)
+    }
  }
+ 
