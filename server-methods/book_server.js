22f1f80e26033a99b85017f2ce27b328971e66a6
tony555t <your-email@example.com>
2025-08-19 16:04:12 +0300

    function get all books

--- server-methods/book_server.js
+++ server-methods/book_server.js
@@ -21,4 +21,13 @@ const http = require ('http');
         deleteBook(req,res)
     }
  }
- 
+ function getAllBooks(req,res){
+    fs.readFile(booksDbpath,"utf8",(err,data)=>{
+        if (err){
+            console.log(err)
+            res.writeHead(400)
+            res.end("an error occcured")
+        }
+        res.end(data)
+    })
+ }
