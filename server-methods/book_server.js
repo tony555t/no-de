3d7f44648e0cf9438b59868e8bf8906e0f786a7f
tony555t <your-email@example.com>
2025-08-19 15:49:46 +0300

    server

---
+++ server-methods/book_server.js
@@ -0,0 +1,13 @@
+const http = require ('http');
+ const fs = require ('fs');
+ const path = require('path');
+
+
+ const booksDbpath = path.join(__dirname,"db",'books.json');
+
+ const PORT =4000 ;
+ const HOST_NAME = 'localhost';
+
+ function requestHandler (req,res){
+    
+ }
